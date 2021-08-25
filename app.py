9fdf5086eb12cd82df7f85cea59db530448d60c5
Kevin Chu <kvnchu@ucdavis.edu>
2021-08-24 20:01:14 -0700

    'Seperating_functions'

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ import dash_html_components as html
 
 import sys
 sys.path.insert(1, 'dash_templates')
-from data_table import data_preview_app_layout #type: ignore
-from dashboard import dashboard_app_layout  #type: ignore
+from nav import create_navbar #type: ignore
+from data_table import generate_table #type: ignore
+from dashboard import  cleaned_data_reader, heatmap_generator, top_states_generator, top_cities_generator, incidents_per_day_generator, incidents_per_month_generator, incidents_per_year_generator, age_distribution_generator, gun_type_distribution_generator, gun_count_distribution_generator, suspect_gender_distribution_generator, victim_gender_distribution_generator#type: ignore
+
+
+# Load in Data
+data = cleaned_data_reader()
 
 
 # Initialize Flask and Dash 
@@ -45,9 +50,77 @@ def contact():
 
 
 # Dash layouts
-data_preview_app.layout = data_preview_app_layout
+generated_table = generate_table()
 
-dashboard_app.layout = dashboard_app_layout
+data_preview_app.layout = html.Div(
+    children = [
+        create_navbar(),
+        html.Br(),
+        generated_table
+    ]
+)
+
+
+# dashboard_app.layout = dashboard_app_layout
+
+incident_heatmap = heatmap_generator(data)
+top_states = top_states_generator(data)
+top_cities = top_cities_generator(data)
+incidents_per_day = incidents_per_day_generator(data)
+incidents_per_month = incidents_per_month_generator(data)
+incidents_per_year = incidents_per_year_generator(data)
+age_distribution = age_distribution_generator(data)
+gun_type_distribution = gun_type_distribution_generator(data)
+gun_count_distribution = gun_count_distribution_generator(data)
+suspect_gender_distribution = suspect_gender_distribution_generator(data)
+victim_gender_distribution = victim_gender_distribution_generator(data)
+
+dashboard_app.layout = html.Div(
+    children = [
+        create_navbar(),
+
+        html.Div(
+            className = 'heatmap-state-city-container ',
+            children = [
+                incident_heatmap,
+
+                html.Div(
+                    className = 'state-city-container',
+                    children = [
+                        top_states,
+                        top_cities
+                    ]
+                )
+            ]
+        ),
+
+        html.Div(
+            className = 'time-container',
+            children = [
+                incidents_per_day,
+                incidents_per_month,
+                incidents_per_year
+            ]
+        ),
+
+        html.Div(
+            className = 'age-distribution-container',
+            children = [
+                age_distribution
+            ]
+        ),
+
+        html.Div(
+            className = 'gender-gun-container',
+            children = [
+                gun_type_distribution,
+                gun_count_distribution,
+                suspect_gender_distribution,
+                victim_gender_distribution
+            ],
+        ),
+    ]
+)
 
 
 
